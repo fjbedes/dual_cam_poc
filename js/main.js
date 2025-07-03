feaf8441025e82fbf6a34ca80845320324f4476e
Francis Jay Bedes <jay@jay.local>
2025-07-03 14:27:27 +0800

    test

--- js/main.js
+++ js/main.js
@@ -1,76 +1,84 @@
-// Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
+let devices = [];
+let currentStreams = [null, null];
 
-// Use of this source code is governed by a BSD-style license
-// that can be found in the LICENSE file in the root of the source
-// tree. An additional intellectual property rights grant can be found
-// in the file PATENTS.  All contributing project authors may
-// be found in the AUTHORS file in the root of the source tree.
-
-"use strict";
-
-var deviceList = [];
-var counter = 0;
-
-window.onload = function () {
-  getSources_();
-};
-
-function getSources_() {
-  navigator.mediaDevices.enumerateDevices().then(function (devices) {
-    for (var i = 0; i < devices.length; i++) {
-      if (devices[i].kind === "videoinput") {
-        deviceList[i] = devices[i];
-        requestVideo_(deviceList[i].deviceId);
-      }
-    }
-  });
-}
-
-function requestVideo_(id) {
-  navigator.mediaDevices
-    .getUserMedia({
-      // Remove the "exact deviceId" constraint:
+async function init() {
+  try {
+    // Request permission first to get device labels
+    const initialStream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: false,
-    })
-    .then(function (stream) {
-      getUserMediaOkCallback_(stream);
-    }, getUserMediaFailedCallback_);
-}
+    });
 
-function getUserMediaFailedCallback_(error) {
-  alert("User media request denied with error: " + error.name);
-}
+    // Stop the initial stream if you don't want to display it
+    initialStream.getTracks().forEach((track) => track.stop());
 
-function getUserMediaOkCallback_(stream) {
-  var videoArea = document.getElementById("videoArea");
-  var video = document.createElement("video");
-  var div = document.createElement("div");
-  div.style.float = "left";
-  video.setAttribute("id", "view" + counter);
-  video.width = 320;
-  video.height = 240;
-  video.autoplay = true;
-  div.appendChild(video);
-  videoArea.appendChild(div);
-  if (typeof stream.getVideoTracks()[0].label !== "undefined") {
-    var deviceLabel = document.createElement("p");
-    deviceLabel.innerHTML = stream.getVideoTracks()[0].label;
-    div.appendChild(deviceLabel);
+    await enumerateCameras();
+  } catch (err) {
+    showError(err);
   }
-  stream.getVideoTracks()[0].addEventListener("ended", errorMessage_);
-  document.getElementById("view" + counter).srcObject = stream;
-  counter++;
 }
 
-var errorMessage_ = function (event) {
-  var message =
-    "getUserMedia successful but " +
-    event.type +
-    " event fired " +
-    "from camera. Most likely too many cameras on the same USB " +
-    "bus/hub. Verify this by disconnecting one of the cameras " +
-    "and try again.";
-  document.getElementById("messages").innerHTML +=
-    event.target.label + ": " + message + "<br><br>";
-};
+async function enumerateCameras() {
+  devices = (await navigator.mediaDevices.enumerateDevices()).filter(
+    (d) => d.kind === "videoinput"
+  );
+
+  const selects = [
+    document.getElementById("cameraSelect1"),
+    document.getElementById("cameraSelect2"),
+  ];
+
+  selects.forEach((select) => {
+    select.innerHTML = "";
+    devices.forEach((device, idx) => {
+      const option = document.createElement("option");
+      option.value = device.deviceId;
+      option.text = device.label || `Camera ${idx + 1}`;
+      select.appendChild(option);
+    });
+  });
+
+  // Auto start the first device in both slots
+  if (devices.length > 0) {
+    startStream(0, devices[0].deviceId);
+  }
+  if (devices.length > 1) {
+    startStream(1, devices[1].deviceId);
+  }
+}
+
+async function startStream(slot, deviceId) {
+  try {
+    if (currentStreams[slot]) {
+      currentStreams[slot].getTracks().forEach((t) => t.stop());
+    }
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { deviceId: { exact: deviceId } },
+      audio: false,
+    });
+
+    currentStreams[slot] = stream;
+    const video = document.getElementById(`video${slot + 1}`);
+    video.srcObject = stream;
+  } catch (err) {
+    showError(err);
+  }
+}
+
+function showError(err) {
+  const msg = document.getElementById("messages");
+  msg.innerHTML = `Error: ${err.name} - ${err.message}`;
+  console.error(err);
+}
+
+document.getElementById("cameraSelect1").addEventListener("change", (e) => {
+  startStream(0, e.target.value);
+});
+
+document.getElementById("cameraSelect2").addEventListener("change", (e) => {
+  startStream(1, e.target.value);
+});
+
+// Start on load
+init();
